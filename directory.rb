aafabc68c1f368ec2eb9d65114432e8fb8f93c9f
terminalobject <doctorboredom@hotmail.com>
2017-05-30 13:49:21 +0200

    interactive menu added

--- directory.rb
+++ directory.rb
@@ -12,21 +12,41 @@ def input_students
 end
 
 def print_header
-	puts "The students of Villains Academy"
-	puts "-------------"
-end 
-
-def print(students)
-	students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
-	end
-end 
-
-def print_footer(students)
-puts "Overall, we have #{students.count} great students"
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
+
+def interactive_menu
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" 
+    selection = gets.chomp
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit 
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
+students = interactive_menu

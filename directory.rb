7fe6cf6ca1a769a767f3a9950bf43e7b85214105
terminalobject <doctorboredom@hotmail.com>
2017-05-31 12:16:17 +0200

    file saving added

--- directory.rb
+++ directory.rb
@@ -10,6 +10,16 @@ def input_students
   end
 end
 
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
@@ -28,6 +38,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit" 
 end 
 
@@ -47,6 +58,8 @@ def interactive_menu
       students = input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit 
     else
